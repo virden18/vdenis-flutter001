729e2077509533aea2513da9732f5870e3a61f99
Diego <die10.2005@gmail.com>
2025-04-01 08:57:09 -0300

    Agregar boton de cotizaciones

--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -8,9 +8,7 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bienvenido'),
-      ),
+      appBar: AppBar(title: const Text('Bienvenido')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,

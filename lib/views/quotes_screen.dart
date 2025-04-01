c83bb9df4f33fc8748822277b8e4c58b839b6057
Chucky2010 <sebas10ibarra81@gmail.com>
2025-04-01 09:48:01 -0300

    modificacion Isaias 2

--- lib/views/quotes_screen.dart
+++ lib/views/quotes_screen.dart
@@ -5,12 +5,29 @@ class QuotesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Lista de cotizaciones ficticias de monedas en relación al guaraní
+    final List<Map<String, String>> quotes = [
+      {'symbol': 'USD', 'price': '7,200 PYG'}, // Dólar estadounidense
+      {'symbol': 'EUR', 'price': '8,000 PYG'}, // Euro
+      {'symbol': 'BRL', 'price': '1,400 PYG'}, // Real brasileño
+      {'symbol': 'ARS', 'price': '50 PYG'},    // Peso argentino
+      {'symbol': 'CLP', 'price': '10 PYG'},    // Peso chileno
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cotizaciones'),
+        title: const Text('Cotizaciones de Monedas'),
       ),
-      body: const Center(
-        child: Text('Aquí se mostrarán las cotizaciones'),
+      body: ListView.builder(
+        itemCount: quotes.length,
+        itemBuilder: (context, index) {
+          final quote = quotes[index];
+          return ListTile(
+            leading: const Icon(Icons.attach_money),
+            title: Text(quote['symbol']!),
+            subtitle: Text('Precio: ${quote['price']}'),
+          );
+        },
       ),
     );
   }
